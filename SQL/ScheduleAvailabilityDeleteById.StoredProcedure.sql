8eb74aadaf53e9dddb98a13859350031c39d1492
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2022-10-22 13:38:11 -0700

    Rename ScheduleAvailability_Delete_ByIdV2.StoredProcedure.sql to ScheduleAvailabilityDeleteById.StoredProcedure.sql

---
+++ SQL/ScheduleAvailabilityDeleteById.StoredProcedure.sql
@@ -0,0 +1,47 @@
+﻿USE [MiVet]
+GO
+/****** Object:  StoredProcedure [dbo].[ScheduleAvailability_Delete_ByIdV2]    Script Date: 10/21/2022 3:24:50 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Brian Cao
+-- Create date: 10/14/2022
+-- Description: Deleting ScheduleAvailability By Id
+-- Code Reviewer:
+
+-- MODIFIED BY: author
+-- MODIFIED DATE:12/1/2020
+-- Code Reviewer:
+-- Note:
+-- =============================================
+CREATE proc [dbo].[ScheduleAvailability_Delete_ByIdV2]
+													@Id int
+
+as
+
+/*
+
+	Declare @Id int = 1;
+
+	Select *
+	From dbo.ScheduleAvailabilityV2
+	Where Id = @Id;
+
+	Execute dbo.ScheduleAvailability_Delete_ByIdV2 @Id
+
+	Select *
+	From dbo.ScheduleAvailabilityV2
+	Where Id = @Id;
+
+*/
+
+BEGIN
+
+	DELETE FROM [dbo].[ScheduleAvailabilityV2]
+	WHERE Id = @Id;
+
+END
+GO
